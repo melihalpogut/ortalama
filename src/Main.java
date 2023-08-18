7584321ea76eced9500e5a235a8dbf78ade60563
melihalpogut <melihalpagut@gmail.com>
2023-08-18 23:42:27 +0300

    ortalama java

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //Değişkenler
+        int mat,fizik,kimya,turkce,tarih,muzik;
+
+        //Scanner sınıfı tanım
+        Scanner inp = new Scanner(System.in);
+
+        //Kullanıcı Değer Alma
+        System.out.println("Matematik Notu:");
+        mat= inp.nextInt();
+
+        System.out.println("Fizik Notu:");
+        fizik= inp.nextInt();
+
+        System.out.println("Kimya Notu:");
+        kimya= inp.nextInt();
+
+        System.out.println("Türkçe Notu:");
+        turkce= inp.nextInt();
+
+        System.out.println("Tarih Notu:");
+        tarih= inp.nextInt();
+
+        System.out.println("Müzik Notu:");
+        muzik= inp.nextInt();
+
+        int toplam = (mat+fizik+kimya+turkce+tarih+muzik);
+        double sonuc = toplam / 6.0;
+        System.out.println("Ortalamanız: " + sonuc);
+    }
+}
